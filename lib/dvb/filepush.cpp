a02393502b8c82576da19f36fbf87a4ee6b6b957
adjik-l <57148174+adjik-l@users.noreply.github.com>
2025-11-05 13:27:17 +0200

    Remove unused parameter from message pump initialization

--- lib/dvb/filepush.cpp
+++ lib/dvb/filepush.cpp
@@ -359,7 +359,7 @@ eFilePushThreadRecorder::eFilePushThreadRecorder(unsigned char* buffer, size_t b
 	m_buffer(buffer),
 	m_overflow_count(0),
 	m_stop(1),
-	m_messagepump(eApp, 0, "eFilePushThreadRecorder")
+	m_messagepump(eApp, 0)
 {
 	m_protocol = m_stream_id = m_session_id = m_packet_no = 0;
 	CONNECT(m_messagepump.recv_msg, eFilePushThreadRecorder::recvEvent);
